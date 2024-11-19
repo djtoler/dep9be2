59b4e4a11673afd521251ce57b790ae74ce26519
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2024-11-19 11:47:39 -0500

    Update urls.py

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from product import views
+from django.http import JsonResponse
 
+def api_root(request):
+    return JsonResponse({
+        "products": "/api/products/",
+        "product-detail": "/api/product/<id>/",
+        "product-create": "/api/product-create/",
+        "product-update": "/api/product-update/<id>/",
+        "product-delete": "/api/product-delete/<id>/",
+    })
 
 urlpatterns = [
+    path('', api_root, name="api-root"),  # Add this line for `/api/`
     path('products/', views.ProductView.as_view(), name="products-list"),
     path('product/<str:pk>/', views.ProductDetailView.as_view(), name="product-details"),
     path('product-create/', views.ProductCreateView.as_view(), name="product-create"),
